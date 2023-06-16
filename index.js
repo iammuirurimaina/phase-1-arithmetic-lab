200636393f76cc04b49b976fad96dc1e5272baeb
ian <ian.maina@student.moringaschool.com>
2023-06-16 14:47:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 62;
+let num2 = 1
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 2;
+let num3 = 10
+let num4 = 6
+let mod = (num3 % num4)
+let max = Math.max(5,9,15,19,20)
